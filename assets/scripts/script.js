9e86f8f60c0344a59f71d5303402f357901aa996
RaihanAkter03 <raihanakter26@gmail.com>
2020-10-24 11:06:24 -0500

    again commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+
+
+    var searchInput = $("#search-city").val().trim();
+    console.log(searchInput);
+
+    // var queryURL = "https://api.airvisual.com/v2/states?country=usa&key=bc4dec27-7130-4a22-88ca-f37ecbcfc5f9";
+    // http://api.airvisual.com/v2/nearest_city?key=your_key
+
+    // var queryURL = "https://api.airvisual.com/v2/nearest_city?key=bc4dec27-7130-4a22-88ca-f37ecbcfc5f9";
+    // var queryURL = "https://api.weatherbit.io/v2.0/current/airquality?city=kansas city&postal_code=64106&country=US&key=e5f946832cc34874b9250ae7016416e0";
+
+    // $.ajax({
+    //     type: "GET",
+    //     url: queryURL,
+    //     dataType: "json",
+    //     success: function (response) {
+    //         console.log(response);
+
+    //     }
+    // });
+
+
+
+
+
+
+
+
+});
