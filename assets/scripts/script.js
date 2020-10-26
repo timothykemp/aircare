289776048c06f58bd40bddd5d8789da0a8f89367
RaihanAkter03 <raihanakter26@gmail.com>
2020-10-26 10:00:35 -0500

    add city list and remove button to remove the city

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,19 +1,42 @@
 
 $(document).ready(function () {
+   
+    var arr = JSON.parse(localStorage.getItem("city")) || [];
 
-
-
-    function locationSearch() { 
         $("#search-city").on("click", function (event) {
             event.preventDefault();
             var searchInput = $("#city-text").val().trim();
             console.log(searchInput);
+            if (arr.indexOf(searchInput) === -1) { 
+                createCitylist(searchInput);
+                arr.push(searchInput);
+                localStorage.setItem("city", JSON.stringify(arr));
+            }
 
-        })
-    }
-    locationSearch();
+        });
     
+    function createCitylist(name) { 
+        var id = 'btn' + name
+        var li = $("<li >");
+        var btn = $('<button />', {
+            text: 'remove',
+            id: id,
+            click: function () {
+                $(this).parent().hide();
+                var localData = JSON.parse(localStorage.getItem("city"));
+                if (localData) {
+                    var data = localData.filter(city => city !== name);
+                    localStorage.removeItem('city');
+                    localStorage.setItem('city', JSON.stringify(data));
+                }
+            }
+        });
+        li.addClass("city-list-item");
+        li.text(name).append(btn);
+        $("#city-list").append(li);
+    }
 
+  
 
     // var queryURL = "https://api.airvisual.com/v2/states?country=usa&key=bc4dec27-7130-4a22-88ca-f37ecbcfc5f9";
     // http://api.airvisual.com/v2/nearest_city?key=your_key
