eb0e15d1c3e58b7c323d55f326a337924c03335b
David French <David@mefalf.net>
2020-10-26 18:15:19 -0500

    adding covid metrics

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,25 +10,7 @@ $(document).ready(function () {
     }
     locationSearch();
 
-  
-$("#searchTerm").keypress(function(event) { 
-	
-	if (event.keyCode === 13) { 
-		event.preventDefault();
-		$("#searchBtn").click(); 
-	} 
-});
-
-$("#searchBtn").on("click", function() {
-
-  $('#covidTrackingH5').addClass('show');
-
-  city = $("#searchTerm").val();
-  
-  $("#searchTerm").val("");  
-});
-
-var queryURL = " https://api.covidtracking.com/v1/us/current.json";
+var queryURL = "https://api.covidtracking.com/v1/us/current.json";
 
 $.ajax({
     type: "GET",
@@ -39,28 +21,33 @@ $.ajax({
     }
 });
 
+then(function (response){
+
+   getCurrentConditions(response);
+
+   })
+
 function getCurrentConditions (response) {
 
-    $('#currentCity').empty();
+    $('#search-city').empty();
 
      const card = $("<div>").addClass("card");
      const city = $("<h4>").addClass("card-title").text(response.name);
      const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
-     const positive = $("<p>").addClass("card-text current-positive").text("positive: ");
-     const negitive = $("<p>").addClass("card-text current-negitive").text("negitive: ");
-     const pending = $("<p>").addClass("card-text current-pending").text("pending: ");
-     const death = $("<p>").addClass("card-text current-death").text("death: ");
-     const recovered = $("<p>").addClass("card-text current-recovered").text("recovered: ");
+     const positive = $("<p>").addClass("card-text current-positive" + response);
+     const negitive = $("<p>").addClass("card-text current-negitive" + response);
+     const pending = $("<p>").addClass("card-text current-pending" + response);
+     const death = $("<p>").addClass("card-text current-death" + response);
+     const recovered = $("<p>").addClass("card-text current-recovered" + response);
 
      
     city.append(cityDate)
     card.append(city, positive, negitive, pending, death, recovered);
-    $("#currentCity").append(card)
+    $("#search-city").append(card)
    
   }
 
-
-
+ 
     // var queryURL = "https://api.airvisual.com/v2/states?country=usa&key=bc4dec27-7130-4a22-88ca-f37ecbcfc5f9";
     // http://api.airvisual.com/v2/nearest_city?key=your_key
 
@@ -82,6 +69,6 @@ function getCurrentConditions (response) {
 
 
 
-
+  
 
 });
